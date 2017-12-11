470decffa4442dc361d94a2977dab4ae3f149f30
Alibek Omarov <a1ba.omarov@gmail.com>
2017-12-11 20:00:46 +0300

    Add missing include

--- Android.mk
+++ Android.mk
@@ -9,8 +9,8 @@ include $(XASH3D_CONFIG)
 
 LOCAL_MODULE := filesystem_stdio
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
-	
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include $(XASH3D_SRC)/engine
+
 LOCAL_CPPFLAGS += -std=c++0x
 
 LOCAL_SRC_FILES := src/filesystem_impl.cpp src/interface.cpp
